e0d831e878e5fac461248842aec45e5cc762aff3
Nathan Hensher <nhensh@gmail.com>
2024-10-09 13:59:56 +0100

    fix: remove test buid js files

--- src/icons/BinIcon.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-var TrashIcon = function (_a) {
-    var className = _a.className;
-    return (React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 24 24", className: className },
-        React.createElement("path", { d: "M13.5 2C14.879 2 16 3.044 16 4.328V6H21C21.55 6 22 6.45 22 7C22 7.55 21.55 8 21 8H20V19C20 20.654 18.654 22 17 22H7C5.346 22 4 20.654 4 19V8H3C2.45 8 2 7.55 2 7C2 6.45 2.45 6 3 6H8V4.328C8 3.044 9.121 2 10.5 2H13.5ZM18 8H6V19C6 19.551 6.448 20 7 20H17C17.552 20 18 19.551 18 19V8ZM9 11C9.55 11 10 11.45 10 12V16C10 16.55 9.55 17 9 17C8.45 17 8 16.55 8 16V12C8 11.45 8.45 11 9 11ZM15 11C15.55 11 16 11.45 16 12V16C16 16.55 15.55 17 15 17C14.45 17 14 16.55 14 16V12C14 11.45 14.45 11 15 11Z", fill: "currentColor" })));
-};
-export default TrashIcon;
